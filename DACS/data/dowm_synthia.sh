c685e0f40da50dff640bd462f3d500235f140610
ygjwd12345 <641807447@qq.com>
2021-07-09 08:43:45 +0200

    v0

---
+++ DACS/data/dowm_synthia.sh
@@ -0,0 +1,5 @@
+wget http://synthia-dataset.net/download/1137/train.zip
+wget http://synthia-dataset.net/download/1137/test.zip
+
+unzip train.zip
+unzip test.zip
---
+++ DACS/data/down_gta.sh
@@ -0,0 +1,44 @@
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/01_images.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/02_images.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/03_images.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/04_images.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/05_images.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/06_images.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/07_images.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/08_images.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/09_images.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/10_images.zip --no-check-certificate
+#
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/01_labels.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/02_labels.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/03_labels.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/04_labels.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/05_labels.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/06_labels.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/07_labels.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/08_labels.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/09_labels.zip --no-check-certificate
+wget https://download.visinf.tu-darmstadt.de/data/from_games/data/10_labels.zip --no-check-certificate
+#
+unzip 01_images.zip
+unzip 02_images.zip
+unzip 03_images.zip
+unzip 04_images.zip
+unzip 05_images.zip
+unzip 06_images.zip
+unzip 07_images.zip
+unzip 08_images.zip
+unzip 09_images.zip
+unzip 10_images.zip
+
+unzip 01_labels.zip
+unzip 02_labels.zip
+unzip 03_labels.zip
+unzip 04_labels.zip
+unzip 05_labels.zip
+unzip 06_labels.zip
+unzip 07_labels.zip
+unzip 08_labels.zip
+unzip 09_labels.zip
+unzip 10_labels.zip
+
